03eec1306b700fc7f2efc70a3bb87d70f6677a95
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2025-04-01 22:54:08 +0530

    Update mail.js

--- server/config/mail.js
+++ server/config/mail.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  
   auth: {
     user: process.env.GOOGLE_EMAIL,
     pass: process.env.GOOGLE_PASSWORD,
