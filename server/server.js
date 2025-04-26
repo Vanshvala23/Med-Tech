5c099708fd13c1087907f219d2d7e189f54e1fdd
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2025-04-27 00:38:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./db/db');
 const authRouter = require('./routes/authRoutes');
 const medicineRouter = require('./routes/medicineRoutes');
+const QRcodeRoutes = require('./routes/QRcodeRoutes');
 const cors = require('cors');
 const app = express();
 connectDB(); // Connect to MongoDB
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors({origin: 'http://localhost:5173'}));
 app.use('/auth', authRouter);
 app.use('/medicine', medicineRouter);
+app.use('/qrcode', QRcodeRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
